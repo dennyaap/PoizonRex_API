d6084f597bdb285b4f3e099ed6e2017c0f0cf7d7
Denis <dennyaa2003@gmail.com>
2023-12-31 02:45:24 +0500

    feat: add product router

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import productRouter from './productRouter.js';
+
+const router = new Router();
+router.use('/product', productRouter);
+
+export default router;
---
+++ routes/productRouter.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import ProductController from '../controllers/ProductController.js';
+
+const router = new Router();
+
+router.get('/', ProductController.getAll);
+router.post('/', ProductController.create);
+
+export default router;
