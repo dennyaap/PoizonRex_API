528979c6b95a23cbeb70ea41a40675faf0ccbb0f
Denis <dennyaa2003@gmail.com>
2023-12-31 05:05:07 +0500

    feat: add receiving product by id

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -3,6 +3,7 @@ import ProductController from '../controllers/ProductController.js';
 
 const router = new Router();
 
+router.get('/:id', ProductController.getOne);
 router.get('/', ProductController.getAll);
 router.post('/', ProductController.create);
 
