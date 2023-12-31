5bd3950b2b01a59d2351ae121f6f555622aae993
Denis <dennyaa2003@gmail.com>
2023-12-31 21:45:58 +0500

    feat: add telegram bot

--- index.js
+++ index.js
@@ -5,17 +5,38 @@ import sequelize from './db.js';
 import router from './routes/index.js';
 import ErrorHandlingMiddleware from './middleware/ErrorHandlingMiddleware.js';
 
+import TelegramBot from 'node-telegram-bot-api';
+
 import { User, Product } from './models/models.js';
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
 app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json());
 app.use('/api', router);
 
 app.use(ErrorHandlingMiddleware);
+
+const token = process.env.BOT_TOKEN;
+const webAppUrl = process.env.WEB_API_URL;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    if (text === '/start') {
+        await bot.sendMessage(chatId, 'Заходи в наш интернет-магазин PoizonRex!', {
+            reply_markup: {
+                inline_keyboard: [[{ text: 'Перейти к покупкам', web_app: { url: webAppUrl } }]],
+            },
+        });
+    }
+});
 
 const start = async () => {
     try {
