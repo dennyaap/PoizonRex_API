39c98d3369ddd418658843be81c03691a779dc9b
Denis <dennyaa2003@gmail.com>
2024-01-06 19:46:51 +0500

    feat: add user controller

---
+++ controllers/UserController.js
@@ -0,0 +1,42 @@
+import { matchedData } from 'express-validator';
+
+import ApiError from '../error/ApiError.js';
+import { User } from '../models/models.js';
+
+import { generateJwt, getUserParameter } from '../utils/auth.js';
+
+const findUser = (queryId) => {
+    return User.findOne({ where: { queryId } });
+};
+class UserController {
+    async registration(req, res, next) {
+        const data = matchedData(req);
+        const queryId = getUserParameter(data.initData, 'query_id');
+
+        if (await findUser(queryId)) {
+            return next(ApiError.badRequest('This user already exists'));
+        }
+        const user = await User.create({ queryId, roleId: 2 });
+
+        const token = generateJwt(user.id, user.roleId);
+
+        return res.json({ token });
+    }
+
+    async login(req, res, next) {
+        const data = matchedData(req);
+        const queryId = getUserParameter(data.initData, 'query_id');
+
+        const user = await findUser(queryId);
+
+        if (!user) {
+            return next(ApiError.notFound('User is not found'));
+        }
+
+        const token = generateJwt(user.id, user.roleId);
+
+        return res.json({ token });
+    }
+}
+
+export default new UserController();
