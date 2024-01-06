69e9485eb9b3486315069de2440d2cb7ac880af4
Denis <dennyaa2003@gmail.com>
2024-01-06 19:56:28 +0500

    feat: add delete and patch methods

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -1,9 +1,36 @@
 import { Router } from 'express';
-import CategoryController from '../controllers/CategoryController.js';
+
+import categoryController from '../controllers/CategoryController.js';
+import {
+    categoryCreateValidation,
+    categoryRemoveValidation,
+    categoryUpdateValidation,
+} from '../validations/category.js';
+
+import CheckRoleMiddleware from '../middleware/CheckRoleMiddleware.js';
+import ValidationMiddleware from '../middleware/ValidationMiddleware.js';
+
+import { roles } from '../config/roles.js';
 
 const router = new Router();
 
-router.get('/', CategoryController.getAll);
-router.post('/', CategoryController.create);
+router.get('/', categoryController.getAll);
+router.post(
+    '/',
+    [CheckRoleMiddleware(roles.ADMIN), categoryCreateValidation, ValidationMiddleware],
+    categoryController.create,
+);
+
+router.delete(
+    '/:id',
+    [CheckRoleMiddleware(roles.ADMIN), categoryRemoveValidation, ValidationMiddleware],
+    categoryController.remove,
+);
+
+router.patch(
+    '/:id',
+    [CheckRoleMiddleware(roles.ADMIN), categoryUpdateValidation, ValidationMiddleware],
+    categoryController.update,
+);
 
 export default router;
