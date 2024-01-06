a4f9f8036b5777693617a11f8357541549bc691c
Denis <dennyaa2003@gmail.com>
2024-01-06 19:45:29 +0500

    feat: add product delete and update

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,42 +1,68 @@
+import { matchedData } from 'express-validator';
 import { Product } from '../models/models.js';
-import { findFilterFields } from '../utils/controller.js';
-// import ApiError from '../error/ApiError.js';
+import ApiError from '../error/ApiError.js';
 
 class ProductController {
-    async create(req, res) {
-        const { title, price, discount, gender, amount, images, categoryId, material, insole, season, country } = req.body;
-        const product = await Product.create({
-            title,
-            price,
-            discount,
-            gender,
-            amount,
-            images,
-            categoryId,
-            material,
-            insole,
-            season,
-            country
-        });
+    async create(req, res, next) {
+        try {
+            const data = matchedData(req);
+            const product = await Product.create(data);
 
-        return res.json(product);
+            return res.json(product);
+        } catch (err) {
+            console.log(err);
+            return next(ApiError.internal('failed to create product'));
+        }
     }
 
-    async getAll(req, res) {
-        const params = req.query;
-        const optionalParams = ['categoryId'];
-        const filterFields = findFilterFields(params, optionalParams);
+    async getAll(req, res, next) {
+        try {
+            const data = matchedData(req);
+            let products = await Product.findAll({ where: data });
 
-        let products = await Product.findAll({ where: filterFields });
-
-        return res.json(products);
+            return res.json(products);
+        } catch (err) {
+            console.log(err);
+            return next(ApiError.internal('failed to get products'));
+        }
     }
 
     async getOne(req, res) {
-        const { id } = req.params;
-        const product = await Product.findOne({ where: { id } });
+        try {
+            const { id } = matchedData(req);
+            const product = await Product.findOne({ where: { id } });
 
-        return res.json(product);
+            return res.json(product);
+        } catch (err) {
+            console.log(err);
+            return next(ApiError.internal('failed to get product'));
+        }
+    }
+
+    async remove(req, res) {
+        try {
+            const { id } = matchedData(req);
+            const product = await Product.destroy({ where: { id } });
+
+            return res.json(product);
+        } catch (err) {
+            console.log(err);
+            return next(ApiError.internal('failed to remove product'));
+        }
+    }
+
+    async update(req, res) {
+        try {
+            const { id } = matchedData(req, { locations: ['param'] });
+            const data = matchedData(req, { locations: ['body'] });
+
+            const product = await Product.update({ where: { id }, data });
+
+            return res.json(product);
+        } catch (err) {
+            console.log(err);
+            return next(ApiError.internal('failed to update product'));
+        }
     }
 }
 
