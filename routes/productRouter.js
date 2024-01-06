1e79e8f28009eb3123556c6aa98f703092c5fb0c
Denis <dennyaa2003@gmail.com>
2024-01-06 19:57:16 +0500

    feat: add delete and patch methods

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,10 +1,40 @@
 import { Router } from 'express';
-import ProductController from '../controllers/ProductController.js';
+
+import productController from '../controllers/ProductController.js';
+import {
+    productGetOneValidation,
+    productGetAllValidation,
+    productCreateValidation,
+    productRemoveValidation,
+    productUpdateValidation,
+} from '../validations/product.js';
+
+import CheckRoleMiddleware from '../middleware/CheckRoleMiddleware.js';
+import ValidationMiddleware from '../middleware/ValidationMiddleware.js';
+
+import { roles } from '../config/roles.js';
 
 const router = new Router();
 
-router.get('/:id', ProductController.getOne);
-router.get('/', ProductController.getAll);
-router.post('/', ProductController.create);
+router.get('/:id', [productGetOneValidation, ValidationMiddleware], productController.getOne);
+
+router.get('/', [productGetAllValidation, ValidationMiddleware], productController.getAll);
+router.post(
+    '/',
+    [CheckRoleMiddleware(roles.ADMIN), productCreateValidation, ValidationMiddleware],
+    productController.create,
+);
+
+router.delete(
+    '/:id',
+    [CheckRoleMiddleware(roles.ADMIN), productRemoveValidation, ValidationMiddleware],
+    productController.remove,
+);
+
+router.patch(
+    '/:id',
+    [CheckRoleMiddleware(roles.ADMIN), productUpdateValidation, ValidationMiddleware],
+    productController.update,
+);
 
 export default router;
