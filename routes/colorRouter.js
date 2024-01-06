3f7987bbe5e42f0455f6348bd12472d4e274c543
Denis <dennyaa2003@gmail.com>
2024-01-06 19:56:49 +0500

    feat: add color router

---
+++ routes/colorRouter.js
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+
+import colorController from '../controllers/ColorController.js';
+import {
+    colorCreateValidation,
+    colorRemoveValidation,
+    colorUpdateValidation,
+} from '../validations/color.js';
+
+import ValidationMiddleware from '../middleware/ValidationMiddleware.js';
+import CheckRoleMiddleware from '../middleware/CheckRoleMiddleware.js';
+
+import { roles } from '../config/roles.js';
+
+const router = new Router();
+
+router.get('/', colorController.getAll);
+router.post(
+    '/',
+    [CheckRoleMiddleware(roles.ADMIN), colorCreateValidation, ValidationMiddleware],
+    colorController.create,
+);
+
+router.delete(
+    '/:id',
+    [CheckRoleMiddleware(roles.ADMIN), colorRemoveValidation, ValidationMiddleware],
+    colorController.remove,
+);
+
+router.patch(
+    '/:id',
+    [CheckRoleMiddleware(roles.ADMIN), colorUpdateValidation, ValidationMiddleware],
+    colorController.update,
+);
+
+export default router;
